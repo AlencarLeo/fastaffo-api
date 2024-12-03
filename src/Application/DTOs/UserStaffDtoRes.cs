bb02aa54066c394795d64d80b4bbaec9c5638696
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2024-12-03 22:33:28 +1100

    add: req, res dto structure

---
+++ src/Application/DTOs/UserStaffDtoRes.cs
@@ -0,0 +1,10 @@
+namespace fastaffo_api.src.Application.DTOs;
+public class UserStaffDtoRes
+{
+
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Phone { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
