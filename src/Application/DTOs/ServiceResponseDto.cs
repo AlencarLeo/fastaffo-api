00f03ae715633bb6525b01e05c6d9fb4724f4cbb
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2025-02-05 23:56:50 +1100

    add: base new serviceResponseDto

--- src/Application/DTOs/ServiceResponseDto.cs
+++ src/Application/DTOs/ServiceResponseDto.cs
@@ -1,15 +1,22 @@
-// public class ServiceResponseDto<T>
 public class ServiceResponseDto
 {
-    // public T? Data { get; set; }
     public string Message { get; set; }
     public int StatusCode { get; set; }
 
-    // public ServiceResponseDto(T? data, string message, int statusCode)
     public ServiceResponseDto(string message, int statusCode)
     {
-        // Data = data;
         Message = message;
         StatusCode = statusCode;
+    }
+}
+
+public class ServiceResponseDto<T> : ServiceResponseDto
+{
+    public T? Data { get; set; }
+
+    public ServiceResponseDto(T? data, string message, int statusCode)
+        : base(message, statusCode)
+    {
+        Data = data;
     }
 }
